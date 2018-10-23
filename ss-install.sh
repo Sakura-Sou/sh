9c3b3257275c50db3539b3ffdc7d265bfa2e4a1e
SakuraChan <Tomeetyouismyfortune@gmail.com>
2018-10-23 18:16:29 +0800

    update ss-install.sh

--- ss-install.sh
+++ ss-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt update
-sudo apt install shadowsocks-libev
+apt update
+apt -y install shadowsocks-libev
 wget https://sakurachan.cn/sh/config.json
 mv -f ./config.json /etc/shadowsocks-libev/config.json
 /etc/init.d/shadowsocks-libev start
